6628c05e05706e0ff60765108fae6621bbfc52ea
Hamuud <artemlask2014@gmail.com>
2023-07-06 20:57:42 +0300

    change router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { App } from './App';
 
 ReactDOM.render(
-  <HashRouter>
+  <Router>
     <App />
-  </HashRouter>,
+  </Router>,
   document.getElementById('root'),
 );
